bf667d16dd1ccde815f899843d990a709befd899
Shubham Maheshwari <45518300+shubhamdhoot333@users.noreply.github.com>
2022-10-08 14:59:13 +0530

    formik_with_react
    formik_with_react in both type work available combine work or unique component work 
    also work woth input field formikContainer ,formikController etc

---
+++ formik_yup/formikwithcombine/src/App.js
@@ -0,0 +1,17 @@
+// import OldYoutubeForm from "./components/OldYoutubeForm";
+// import NewYoutubeForm from "./components/NewYoutubeForm";
+// import Work from "./component1/Work";
+import FormikContainer from "./component2/FormikContainer";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      {/* <Work /> */}
+      {/* <NewYoutubeForm /> */}
+      <FormikContainer />
+    </div>
+  );
+}
+
+export default App;
